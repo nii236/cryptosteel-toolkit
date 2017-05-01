df13684af4e6d65e0dd73db8a6ae0c38a9edf846
John Nguyen <jnguyen@thefrontiergroup.com.au>
2017-05-01 13:08:00 +0800

    refactor and add readme

--- main.go
+++ main.go
@@ -1,65 +1,122 @@
 package main
 
 import (
-	"encoding/hex"
+	"bytes"
+	"crypto/sha256"
 	"flag"
 	"fmt"
 	"log"
 	"strings"
 
+	"github.com/btcsuite/btcutil/base58"
+
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil/hdkeychain"
-	"github.com/nii236/go-bip39"
+	bip39 "github.com/nii236/go-bip39"
 )
 
-var passphrase string
+var mnemonic string
 var password string
 var truncated bool
-var childIndex int
+var generate bool
 
 // Example: pizz knoc taxi bris quar tuna much mang okay twel edge brok occu base corn
 func init() {
-	flag.StringVar(&passphrase, "p", "", "Passphrase")
-	flag.BoolVar(&truncated, "t", false, "Truncated passphrase")
-	flag.StringVar(&password, "w", "", "Password")
-	flag.IntVar(&childIndex, "c", 0, "Child key")
+	flag.StringVar(&mnemonic, "m", "", "Mnemonic")
+	flag.BoolVar(&truncated, "t", false, "Truncated mnemonic")
+	flag.StringVar(&password, "p", "", "Password")
+	flag.BoolVar(&generate, "gen", false, "Generate a mnemonic")
+}
+
+func generateMnemonic() {
+	entropy, err := bip39.NewEntropy(256)
+	if err != nil {
+		panic(err)
+	}
+	mnemonic, err := bip39.NewMnemonic(entropy)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(mnemonic)
+}
+
+func prepareSeed(mnemonic, password string, truncated bool) *hdkeychain.ExtendedKey {
+	seed := bip39.NewSeed(mnemonic, password)
+	fmt.Println("mnemonic:", mnemonic)
+	hdkey, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	child, _ := hdkey.Child(0x80000000 + 44)
+	child, _ = child.Child(0x80000000)
+	child, _ = child.Child(0x80000000)
+	child, _ = child.Child(0)
+	child, _ = child.Child(0)
+
+	return child
+}
+
+func mnemonicToScriptPubKey(child *hdkeychain.ExtendedKey) {
+	scriptpubkey, err := child.Address(&chaincfg.MainNetParams)
+	fmt.Println("Extended Key:", child.String())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("scriptPubKey:", scriptpubkey)
+}
+
+func mnemonicToWIF(child *hdkeychain.ExtendedKey) {
+	privKey, err := child.ECPrivKey()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var finalBytes bytes.Buffer
+	finalBytes.WriteByte(chaincfg.MainNetParams.PrivateKeyID)
+	finalBytes.Write(privKey.Serialize())
+	finalBytes.WriteByte(0x01)
+
+	round1 := sha256.New()
+	round1.Write(finalBytes.Bytes())
+	result1 := round1.Sum(nil)
+
+	round2 := sha256.New()
+	round2.Write(result1)
+	result2 := round2.Sum(nil)
+
+	finalBytes.Write(result2[0:4])
+	wif := base58.Encode(finalBytes.Bytes())
+	fmt.Println("WIF:", wif)
 }
 
 func main() {
 	flag.Parse()
-	passphrase := strings.Split(passphrase, " ")
-	if password != "" {
-		fmt.Println("Password:", password)
+
+	if generate {
+		generateMnemonic()
+		return
 	}
-	mnemonic := []string{}
+
+	mnemonicArray := strings.Split(mnemonic, " ")
+	mnemonicFinalArray := []string{}
+	var mnemonicFinal string
 	if truncated {
-		for _, truncatedWord := range passphrase {
+		fmt.Println("Untruncating mnemonic...")
+		for _, truncatedWord := range mnemonicArray {
+			fmt.Println("Untruncating word:", truncatedWord)
 			untruncatedWord := bip39.TruncatedWordMap[truncatedWord]
 			if bip39.TruncatedWordMap[truncatedWord] == "" {
 				log.Fatalln("No matching word found for:", truncatedWord)
 			}
-			mnemonic = append(mnemonic, untruncatedWord)
+			mnemonicFinalArray = append(mnemonicFinalArray, untruncatedWord)
 		}
+		mnemonicFinal = strings.Join(mnemonicFinalArray, " ")
 	} else {
-		for _, untruncatedWord := range passphrase {
-			mnemonic = append(mnemonic, untruncatedWord)
-		}
+		fmt.Println("Using full mnemonic...")
+		mnemonicFinal = mnemonic
 	}
 
-	seed := bip39.NewSeed(strings.Join(mnemonic, " "), password)
-	fmt.Println("mnemonic:", strings.Join(mnemonic, " "))
-	fmt.Println("seed:", hex.EncodeToString(seed))
-
-	hdkey, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("BIP32 Root Key:", hdkey.String())
-	child, _ := hdkey.Child(uint32(childIndex))
-	scriptpubkey, err := child.Address(&chaincfg.MainNetParams)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("scriptpubkey:", scriptpubkey)
+	child := prepareSeed(mnemonicFinal, password, truncated)
+	mnemonicToScriptPubKey(child)
+	mnemonicToWIF(child)
 }
